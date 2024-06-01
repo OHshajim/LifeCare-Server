906bab86e858c49ab46d4a81373b2bebfe0a9727
OHshajim <ajshajimmax@gmail.com>
2024-06-01 11:02:39 +0600

    mongodb applied

---
+++ README.md
@@ -0,0 +1,6 @@
+**To use code or clone or run this server that**
+1. applied and connect with mongoDB
+        DB_USER= {name} 
+        DB_PASS= {pass}
+
+2. install npm packages ---->{express, cors , dotenv, mongodb}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -10,23 +11,48 @@ app.use(cors());
 app.use(express.json());
 
 
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q9eobgc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // collections 
+        const campsCollections = client.db('LifeCare').collection('Camps');
+        // APIs
+        app.get('/camps', async (req, res) => {
+            const result = await campsCollections.find().toArray();
+            res.send(result) 
+        })
 
 
-
-
-
-
-
-
-
-
-
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
 // server running test
 app.get('/', (req, res) => {
-    res.send('medical Camp server running ... on ', port)
+    res.send('LifeCare server running ...')
 })
 
 app.listen(port, () => {
